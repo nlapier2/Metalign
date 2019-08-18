649e7768934e9ede8dbd1464edac33cff92ea5d9
Nathan LaPierre <nathanl2012@gmail.com>
2019-08-18 11:18:58 -0700

    added organism files to setup script, then split into setup_data and setup_libraries

---
+++ setup_libraries.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/lh3/minimap2.git && cd minimap2 && make && cd ..
+git clone https://github.com/dkoslicki/CMash.git && cd CMash && pip install --user -r requirements.txt && cd ..
+#
