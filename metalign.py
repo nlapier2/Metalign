33f1f66e17de120e614aeccfb0a81f9f118a1549
Nathan LaPierre <nathanl2012@gmail.com>
2019-09-30 18:43:42 -0700

    added --sensitive, --precise, and --low_mem to metalign.py

--- metalign.py
+++ metalign.py
@@ -18,23 +18,30 @@ def metalign_parseargs():  # handle user arguments
 		help = 'Location of db_info file. Default: data/db_info.txt')
 	parser.add_argument('--keep_temp_files', action = 'store_true',
 		help='Keep KMC files instead of deleting after this script finishes.')
-	parser.add_argument('--min_abundance', type=float, default=10**-4,
-		help='Minimum abundance for a taxa to be included in the results.')
 	parser.add_argument('--length_normalize', action='store_true',
 		help='Normalize abundances by genome length.')
-	parser.add_argument('--rank_renormalize', action='store_true',
-		help='Renormalize abundances to 100 percent at each rank,\
-				for instance if an organism has a species but not genus label.')
+	parser.add_argument('--low_mem', action='store_true',
+		help='Run in low memory mode, with inexact multimapped processing.')
+	parser.add_argument('--min_abundance', type=float, default=10**-4,
+		help='Minimum abundance for a taxa to be included in the results.')
+	parser.add_argument('--no_quantify_unmapped', action='store_true',
+		help='Do not factor in unmapped reads in abundance estimation.')
 	parser.add_argument('--output', default='abundances.tsv',
 		help='Output abundances file. Default: abundances.tsv')
 	parser.add_argument('--pct_id', type=float, default=0.5,
 		help='Minimum percent identity from reference to count a hit.')
-	parser.add_argument('--no_quantify_unmapped', action='store_true',
-		help='Do not factor in unmapped reads in abundance estimation.')
+	parser.add_argument('--precise', action = 'store_true',
+		help='Run in precise mode. Overwrites --read_cutoff\
+                and --min_abundance to 100 and 0.1.')
+	parser.add_argument('--rank_renormalize', action='store_true',
+		help='Renormalize abundances to 100 percent at each rank,\
+				for instance if an organism has a species but not genus label.')
 	parser.add_argument('--read_cutoff', type=int, default=1,
 		help='Number of reads to count an organism as present.')
 	parser.add_argument('--sampleID', default='NONE',
 		help='Sample ID for output. Defaults to input file name(s).')
+	parser.add_argument('--sensitive', action = 'store_true',
+		help='Run in sensitive mode. Overwrites --cutoff value to 0.0.')
 	parser.add_argument('--strain_level', action='store_true',
 		help='Use this flag to profile strains (off by default).')
 	parser.add_argument('--temp_dir', default = 'TEMP_metalign/',
@@ -56,6 +63,15 @@ def main():
 		args.data + 'db_info.txt'
 	if args.db_dir == 'AUTO':
 		args.data + 'organism_files/'
+
+    # handle precise and sensitive modes
+    if args.sensitive and args.precise:
+        sys.exit('You cannot use both --sensitive and --precise.')
+    if args.sensitive:
+        args.cutoff = 0.0
+    elif args.precise:
+        args.read_cutoff = 100
+        args.min_abundance = 0.1
 
 	# Ensure that arguments agree between scripts
 	args.db = args.temp_dir + 'cmashed_db.fna'
