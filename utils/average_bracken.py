4e14f6e60ed7e8646e090df3d98acdabe7c8e04c
Nathan LaPierre <nathanl2012@gmail.com>
2019-10-08 18:03:12 -0700

    changed organism files to gzipped and read to zcat; reorganized utility scripts into utils/ folder, added script to average bracken/metaphlan output files

---
+++ utils/average_bracken.py
@@ -0,0 +1,55 @@
+# Takes in a filename pattern matching two or more CAMI-format metagenome
+#  	profiles, averages the taxa abundances across all files, and writes results
+import argparse, glob
+
+
+def parseargs():
+	parser = argparse.ArgumentParser(description="Given filename pattern" +
+				" for CAMI-format profiles, average abundances & output.")
+	parser.add_argument('--pattern', required=True,
+		help='Pattern to match, e.g. "/path/to/profiles/*pattern*". Required.')
+	parser.add_argument('--output', required=True,
+		help='Where to write output profile. Required.')
+	args = parser.parse_args()
+	return args
+
+
+def parse_profile(profile):
+	profile_results = {}
+	with(open(profile, 'r')) as infile:
+		infile.readline()  # skip header
+		for line in infile:
+			splits = line.strip().split('\t')
+			taxon, abundance = splits[0], float(splits[-1])
+			profile_results[taxon] = abundance
+	return profile_results
+
+
+# Writes results out in CAMI format
+def write_results(output, results):
+	with(open(output, 'w')) as outfile:
+		for taxon in results:
+			outfile.write(taxon + '\t' + str(results[taxon]) + '\n')
+
+
+def main():
+	args = parseargs()
+	profiles = glob.glob(args.pattern)
+	num_profiles = float(len(profiles))
+	results = {}
+
+	# get results for each profile and sum them up
+	for pro in profiles:
+		profile_results = parse_profile(pro)
+		for taxon in profile_results:
+			averaged_abundance = profile_results[taxon] / num_profiles
+			if taxon not in results:
+				results[taxon] = averaged_abundance
+			else:
+				results[taxon] += averaged_abundance
+	write_results(args.output, results)
+
+
+if __name__ == '__main__':
+	main()
+#
