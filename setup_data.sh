40cb6e8d1b1c5b48f341f9f1c587f5fd533ea7da
Nathan LaPierre <nathanl2012@gmail.com>
2019-11-17 18:47:17 -0800

    moved data to box

--- setup_data.sh
+++ setup_data.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-fileid="1f_zbd9JqVsIgOe-wwUvytfIBoiTOa9T2"
-filename="db_info.txt.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-rm cookie
-
-fileid="1g0dm01WHY-wPtTozcsSRFpw8jZdXUfLW"
-filename="cmash_files.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-rm cookie
-
-fileid="1dwO4Cjxx2EmjpP0uXiWNpOJvF5F8_jgY"
-filename="organism_files.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-rm cookie
-
-mv db_info.txt.zip cmash_files.zip organism_files.zip data/
-cd data
-unzip db_info.txt.zip
-unzip cmash_files.zip
-unzip organism_files.zip
-rm db_info.txt.zip cmash_files.zip organism_files.zip
-cd ..
-
-#
-# Google drive download method adapted from the following under the Creative Commons License:
-#   https://stackoverflow.com/questions/48133080/how-to-download-a-google-drive-url-via-curl-or-wget
+cd data/
+wget https://ucla.box.com/shared/static/b0d5jqtopgrenmlqrdobv4y4hp7ununx.gz && tar -zxvf b0d5jqtopgrenmlqrdobv4y4hp7ununx.gz && rm b0d5jqtopgrenmlqrdobv4y4hp7ununx.gz 
+mkdir organism_files/ && cd organism_files/
+wget https://ucla.box.com/shared/static/c1g8xjc9glh68oje9e549fjqj0y8nc17.gz && tar -zxvf c1g8xjc9glh68oje9e549fjqj0y8nc17.gz && rm c1g8xjc9glh68oje9e549fjqj0y8nc17.gz
+wget https://ucla.box.com/shared/static/lgj75x6w3ddy9fk0d563m4ohjrlozcle.gz && tar -zxvf lgj75x6w3ddy9fk0d563m4ohjrlozcle.gz && rm lgj75x6w3ddy9fk0d563m4ohjrlozcle.gz 
+wget https://ucla.box.com/shared/static/liggr82n10wh76ugwp8iyr55wj4krv5s.gz && tar -zxvf liggr82n10wh76ugwp8iyr55wj4krv5s.gz && rm liggr82n10wh76ugwp8iyr55wj4krv5s.gz
+wget https://ucla.box.com/shared/static/ur07qr5xr4p3qrg3uusgiiq72v75c8a0.gz && tar -zxvf ur07qr5xr4p3qrg3uusgiiq72v75c8a0.gz && rm ur07qr5xr4p3qrg3uusgiiq72v75c8a0.gz
+wget https://ucla.box.com/shared/static/5jij8q5gptr5b3yukwlrzy7zhcyupcrm.gz && tar -zxvf 5jij8q5gptr5b3yukwlrzy7zhcyupcrm.gz && rm 5jij8q5gptr5b3yukwlrzy7zhcyupcrm.gz 
+wget https://ucla.box.com/shared/static/o8nk2vkuci93zvx5kn3klrk77kz0nbx3.gz && tar -zxvf o8nk2vkuci93zvx5kn3klrk77kz0nbx3.gz && rm o8nk2vkuci93zvx5kn3klrk77kz0nbx3.gz
+wget https://ucla.box.com/shared/static/hicbumgx736bejquay6za6t22gprc0xc.gz && tar -zxvf hicbumgx736bejquay6za6t22gprc0xc.gz && rm hicbumgx736bejquay6za6t22gprc0xc.gz
+wget https://ucla.box.com/shared/static/qi2pc4wp75el5fq7wi7k3ht5512sh7if.gz && tar -zxvf qi2pc4wp75el5fq7wi7k3ht5512sh7if.gz && rm qi2pc4wp75el5fq7wi7k3ht5512sh7if.gz
+wget https://ucla.box.com/shared/static/vfr055tuyl7cmboxjouyk984r8eadzav.gz && tar -zxvf vfr055tuyl7cmboxjouyk984r8eadzav.gz && rm vfr055tuyl7cmboxjouyk984r8eadzav.gz
+wget https://ucla.box.com/shared/static/6dl8eoh84sfl78nsmpi9tckwchnya7eh.gz && tar -zxvf 6dl8eoh84sfl78nsmpi9tckwchnya7eh.gz && rm 6dl8eoh84sfl78nsmpi9tckwchnya7eh.gz
+wget https://ucla.box.com/shared/static/edgfk4ubnktlrbt0a7gjmfzdgovpexmj.gz && tar -zxvf edgfk4ubnktlrbt0a7gjmfzdgovpexmj.gz && rm edgfk4ubnktlrbt0a7gjmfzdgovpexmj.gz
+wget https://ucla.box.com/shared/static/c5iphbjw1snrxtrsc3g1y1x5n2896x3d.gz && tar -zxvf c5iphbjw1snrxtrsc3g1y1x5n2896x3d.gz && rm c5iphbjw1snrxtrsc3g1y1x5n2896x3d.gz
+wget https://ucla.box.com/shared/static/qfgoxhurf92vbrf8k1cfzv8al3acdtfz.gz && tar -zxvf qfgoxhurf92vbrf8k1cfzv8al3acdtfz.gz && rm qfgoxhurf92vbrf8k1cfzv8al3acdtfz.gz
+wget https://ucla.box.com/shared/static/jupbcmmrcvdy4gz6pveceiw649zwn4oy.gz && tar -zxvf jupbcmmrcvdy4gz6pveceiw649zwn4oy.gz && rm jupbcmmrcvdy4gz6pveceiw649zwn4oy.gz
+wget https://ucla.box.com/shared/static/ry7ax7rmzlcwdqeytbscgmp4a06hln4c.gz && tar -zxvf ry7ax7rmzlcwdqeytbscgmp4a06hln4c.gz && rm ry7ax7rmzlcwdqeytbscgmp4a06hln4c.gz
+wget https://ucla.box.com/shared/static/xz9hnnwt2otgbaitwpxb8hzn871okg5z.gz && tar -zxvf xz9hnnwt2otgbaitwpxb8hzn871okg5z.gz && rm xz9hnnwt2otgbaitwpxb8hzn871okg5z.gz
+wget https://ucla.box.com/shared/static/1yavykuz7lsj10uld7utqgkpdiax7wg3.gz && tar -zxvf 1yavykuz7lsj10uld7utqgkpdiax7wg3.gz && rm 1yavykuz7lsj10uld7utqgkpdiax7wg3.gz
+wget https://ucla.box.com/shared/static/ue5cy4pglopllxwb604i9hp46vhspeq5.gz && tar -zxvf ue5cy4pglopllxwb604i9hp46vhspeq5.gz && rm ue5cy4pglopllxwb604i9hp46vhspeq5.gz 
+wget https://ucla.box.com/shared/static/kqn4e2g8g51k09mhi8zhulbijc1f4eec.gz && tar -zxvf kqn4e2g8g51k09mhi8zhulbijc1f4eec.gz && rm kqn4e2g8g51k09mhi8zhulbijc1f4eec.gz
+wget https://ucla.box.com/shared/static/6zvktxry08anztawqtro83ifwnl02zc2.gz && tar -zxvf 6zvktxry08anztawqtro83ifwnl02zc2.gz && rm 6zvktxry08anztawqtro83ifwnl02zc2.gz
+wget https://ucla.box.com/shared/static/kwyt44v0g2ej6fegzvhodmxb0f8m26qx.gz && tar -zxvf kwyt44v0g2ej6fegzvhodmxb0f8m26qx.gz && rm kwyt44v0g2ej6fegzvhodmxb0f8m26qx.gz
+wget https://ucla.box.com/shared/static/auju4hgypoenbp6e2ugg3ivage2qajid.gz && tar -zxvf auju4hgypoenbp6e2ugg3ivage2qajid.gz && rm auju4hgypoenbp6e2ugg3ivage2qajid.gz
+wget https://ucla.box.com/shared/static/b59azs9jvyk505l4a17bafn6e5jkvd9r.gz && tar -zxvf b59azs9jvyk505l4a17bafn6e5jkvd9r.gz && rm b59azs9jvyk505l4a17bafn6e5jkvd9r.gz 
+wget https://ucla.box.com/shared/static/ectkfhzoyh7typde4bgk6xy20tpq7dxe.gz && tar -zxvf ectkfhzoyh7typde4bgk6xy20tpq7dxe.gz && rm ectkfhzoyh7typde4bgk6xy20tpq7dxe.gz
+cd ../..
 #
