e44a649c96d8356ea8ce4fa0ee8c4913daed9b25
Nathan LaPierre <nathanl2012@gmail.com>
2019-12-04 11:02:38 -0800

    removed --user flag in setup_libraries.sh

--- setup_libraries.sh
+++ setup_libraries.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/lh3/minimap2.git && cd minimap2 && make && cd ..
-git clone https://github.com/dkoslicki/CMash.git && cd CMash && pip install --user -r requirements.txt && cd ..
+git clone https://github.com/dkoslicki/CMash.git && cd CMash && pip install -r requirements.txt && cd ..
 git clone https://github.com/refresh-bio/KMC.git  && cd KMC && make && cd ..
 #
