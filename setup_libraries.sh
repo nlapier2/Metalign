e7460af61c35bec93e0d4698dc5bf071db64dd8b
Nathan LaPierre <nathanl2012@gmail.com>
2019-12-10 11:58:58 -0800

    setup_libraries now installs cmash with python2 virtualenv

--- setup_libraries.sh
+++ setup_libraries.sh
@@ -1,4 +1,8 @@
 git clone https://github.com/lh3/minimap2.git && cd minimap2 && make && cd ..
-git clone https://github.com/dkoslicki/CMash.git && cd CMash && pip install -r requirements.txt && cd ..
 git clone https://github.com/refresh-bio/KMC.git  && cd KMC && make && cd ..
+
+python2 -m virtualenv env
+source env/bin/activate
+git clone https://github.com/dkoslicki/CMash.git && cd CMash && pip install -r requirements.txt && cd ..
+deactivate
 #
